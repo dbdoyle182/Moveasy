526907ed4ebf95320f68f5d887d14b6e9d9396df
Juan Fox <foxj0875@gmail.com>
2018-03-13 16:09:24 -0400

    getting city codes

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -1,2 +1,15 @@
 
+var cityId;
 
+
+function getCityId(city) {
+  $.ajax({
+    headers: {'user-key':'c3d9aafa108e4ed6fabcca6ee5c1d954'},
+    url: 'https://developers.zomato.com/api/v2.1/cities?q=' + city,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+  });
+}
+
+getCityId('charlotte nc');
