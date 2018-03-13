5983da7c190833b0d3cff4035474d96a7824cd40
Brad Ferrell <33673313+bradbotcode@users.noreply.github.com>
2018-03-13 14:14:55 -0400

    favCity logic
    favCity logic

---
+++ assets/javascript/fav cities.js
@@ -0,0 +1,27 @@
+$(".favCity").click(function(event) {
+  event.preventDefault();
+
+  if (!user) {
+    alert("must be logged in to favorite a city");
+    return;
+  }
+
+  var uid = user.uid;
+  var myCitiesRef = database.ref("/" + uid + "/favCities");
+
+  myCitiesRef.once("value", function(snap) {
+    // get current favs from firebase
+    var favs = snap.val();
+
+    // if no value in firebase
+    if (!Array.isArray(favs)) {
+      // start with empty array
+      favs = [];
+    }
+
+    var cityName = $(".cityTitle").text();
+    favs.push(cityName);
+
+    myCitiesRef.set(favs);
+  });
+});
