ce236936d865611f95492ceac1611dd0da90d6ab
Juan Fox <foxj0875@gmail.com>
2018-03-14 10:42:23 -0400

    api call for location details is creating an array of best rated restaurant objects with the keys that we want to use

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -1,5 +1,5 @@
 
-var cityId;
+
 
 
 function getCityId(city) {
@@ -7,8 +7,32 @@ function getCityId(city) {
     headers: {'user-key':'c3d9aafa108e4ed6fabcca6ee5c1d954'},
     url: 'https://developers.zomato.com/api/v2.1/cities?q=' + city,
     method: 'GET'
-  }).then(function(response) {
-    console.log(response);
+  }).then(function(firstResponse) {
+    var cityId = firstResponse.location_suggestions[0].id;
+
+    console.log(cityId);
+
+    $.ajax({
+      headers: {'user-key':'c3d9aafa108e4ed6fabcca6ee5c1d954'},
+      url: 'https://developers.zomato.com/api/v2.1/location_details?entity_type=city&entity_id=' + cityId,
+      method: 'GET'
+    }).then(function(secondResponse) {
+      var bestRatedRestaurants = secondResponse.best_rated_restaurant;
+
+      var arrayOfRestaurantObjs = bestRatedRestaurants.map(function(item) {
+        console.log(item)
+        var obj = {};
+        var restaurant = item.restaurant;
+
+        obj.name = restaurant.name;
+        obj.address = restaurant.location.address;
+        obj.imgURL = restaurant.featured_image;
+
+        return obj;
+      });
+
+      console.log(arrayOfRestaurantObjs);
+    });
   });
 }
 
